7b484798126e1135c01f15a1d9257263953f84d3
Nhat Duy <duy2808abc@gmail.com>
2026-01-29 18:10:20 +0700

    update router/AppRouter.jsx
    Signed-off-by: Nhat Duy <duy2808abc@gmail.com>

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Messages from "../views/Messages/Messages";
+import SocialLayout from "../layouts/SocialLayout/SocialLayout";
+import Feed from "../views/Social/Feed/Feed";
+import Profile from "../views/Social/Profile/Profile";
 import Login from "../views/Auth/Login/Login";
 import Register from "../views/Auth/Register/Register";
 import ProtectedRoute from "./ProtectedRoute";
@@ -9,11 +12,18 @@ function AppRouter() {
     return (
         <Routes>
             {/* Trang gốc */}
-            <Route path="/" element={<Navigate to="/messages" replace />} />
+            <Route path="/" element={<Navigate to="/feed" replace />} />
 
             {/* Protected (cần login) */}
             <Route element={<ProtectedRoute />}>
                 <Route path="/messages" element={<Messages />} />
+
+                {/* Social UI (Facebook-like) */}
+                <Route element={<SocialLayout />}>
+                    <Route path="/feed" element={<Feed />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/profile/:id" element={<Profile />} />
+                </Route>
             </Route>
 
             {/* Public (chỉ cho CHƯA login) */}
